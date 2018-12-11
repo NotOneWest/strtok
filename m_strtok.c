d26bccde86829722ab5ebd995046b8bd7faf6a79
duncan2121 <42708815+duncan2121@users.noreply.github.com>
2018-12-11 14:40:41 +0900

    Update m_strtok.c

--- m_strtok.c
+++ m_strtok.c
@@ -1,20 +1,25 @@
 #include <stdio.h>
+#include <string.h>
 
 void m_strtok(char *str1, char *str2){
 	
 	int check =0;
 	
-	for(int i=0; str1[i]!='\0'; i++){
+	for(int i=0; i<strlen(str1)-1;){
 		for (int j=0; str2[j] != '\0'; j++){
-			if(str1[i] == str2[j]){
-				check = 1;
+			if(str1[i+j] == str2[j]){
+				check++;
 			}
 		}
-		if(check == 1){
-				printf("\n");
-				check = 0;
-			}
-		else printf("%c",str1[i]);
+		if(check == strlen(str2)-1) {
+			printf("\n");
+			i+=strlen(str2)-1;
+			check = 0;
+		}
+		else {
+			printf("%c",str1[i]);
+			i++;
+		}
 	}
 	
 }
@@ -22,12 +27,10 @@ void m_strtok(char *str1, char *str2){
 int main(){
 	char str1[1000], str2[1000];
 	
-	gets(str1);
-	gets(str2);
+	fgets(str1,sizeof(str1),stdin);
+	fgets(str2,sizeof(str2),stdin);
 		
 	m_strtok(str1,str2);
 	
 	return 0;
 }
-
-
